b4e012040724e61ce6587273bc375a14bb1be53a
Rishav Jain <rishav.j@gmail.com>
2016-08-07 21:39:52 +0100

    edited script

--- code/create-np-vecs.bash
+++ code/create-np-vecs.bash
@@ -1,26 +1,35 @@
 #!/bin/bash
 
 EVAL=/home/cop15rj/rishav-msc-project/evaluate/lexsub-master
-DATA=/data/cop15rj/lexsub/1
-OUT=/data/cop15rj/lexsub/1.2
-LOG=/data/cop15rj/lexsub/1.2
+DATA=/data/cop15rj/lexsub/2.1
+DATA=/data/cop15rj/lexsub/3
+OUT=/data/cop15rj/lexsub/2.1
+OUT=/data/cop15rj/lexsub/3.1
+LOG=/data/cop15rj/lexsub/2.1
+LOG=/data/cop15rj/lexsub/3.1
 SCRIPTS=/home/cop15rj/rishav-msc-project/code
-export PYTHONPATH='/home/cop15rj/rishav-msc-project/*:/home/cop15rj/rishav-msc-project/evaluate/lexsub-master/*:'
+#export PYTHONPATH='/home/cop15rj/rishav-msc-project/evaluate/lexsub-master/*:'
 
 mkdir -p $OUT
 
-#CMD1="python $EVAL/jcs/text2numpy.py $DATA/lexsub_word_embeddings $OUT/vecs"
+#CMD1="python $EVAL/jcs/text2numpy.py $DATA/dim600vecs $OUT/vecs"
 ##echo qsub -l mem=48G -l rmem=16G -j y -o $LOG/vecs.log -N text2numpy-vecs $SCRIPTS/redirect.bash $CMD1
 #echo $CMD1
-#$CMD1
+##$CMD1
 #
-#CMD2="python $EVAL/jcs/text2numpy.py $DATA/lexsub_context_embeddings $OUT/contexts"
+#CMD2="python $EVAL/jcs/text2numpy.py $DATA/dim600contexts $OUT/contexts"
 ##echo qsub -l mem=48G -l rmem=16G -j y -o $LOG/contexts.log -N text2numpy-contexts $SCRIPTS/redirect.bash $CMD2
 #echo $CMD2
-#$CMD2
+##$CMD2
 
-echo python $EVAL/jcs/jcs_main.py --inferrer emb -vocabfile $EVAL/datasets/ukwac.vocab.lower.min100 -testfile $EVAL/datasets/lst_all.preprocessed -testfileconll $EVAL/datasets/lst_all.conll -candidatesfile $EVAL/datasets/lst.gold.candidates -embeddingpath $OUT/vecs -embeddingpathc $OUT/contexts -contextmath mult --debug -resultsfile $OUT/results
-python $EVAL/jcs/jcs_main.py --inferrer emb -vocabfile $EVAL/datasets/ukwac.vocab.lower.min100 -testfile $EVAL/datasets/lst_all.preprocessed -testfileconll $EVAL/datasets/lst_all.conll -candidatesfile $EVAL/datasets/lst.gold.candidates -embeddingpath $OUT/vecs -embeddingpathc $OUT/contexts -contextmath mult --debug -resultsfile $OUT/results
+CURR=$(pwd)
 
-#echo python $EVAL/jcs/evaluation/lst/lst_gap.py $EVAL/datasets/lst_all.gold $OUT/results.ranked $OUT/gap no-mwe
-#python $EVAL/jcs/evaluation/lst/lst_gap.py $EVAL/datasets/lst_all.gold $OUT/results.ranked $OUT/gap no-mwe
+cd $EVAL
+
+#echo python jcs/jcs_main.py --inferrer emb -vocabfile $EVAL/datasets/ukwac.vocab.lower.min100 -testfile $EVAL/datasets/lst_all.preprocessed -testfileconll $EVAL/datasets/lst_all.conll -candidatesfile $EVAL/datasets/lst.gold.candidates -embeddingpath $DATA/vecs -embeddingpathc $DATA/contexts -contextmath mult --debug -resultsfile $OUT/results
+#python jcs/jcs_main.py --inferrer emb -vocabfile $EVAL/datasets/ukwac.vocab.lower.min100 -testfile $EVAL/datasets/lst_all.preprocessed -testfileconll $EVAL/datasets/lst_all.conll -candidatesfile $EVAL/datasets/lst.gold.candidates -embeddingpath $DATA/vecs -embeddingpathc $DATA/contexts -contextmath mult --debug -resultsfile $OUT/results
+
+echo python jcs/evaluation/lst/lst_gap.py $EVAL/datasets/lst_all.gold $OUT/results.ranked $OUT/gap no-mwe
+python jcs/evaluation/lst/lst_gap.py $EVAL/datasets/lst_all.gold $OUT/results.ranked $OUT/gap no-mwe
+
+cd $CURR
